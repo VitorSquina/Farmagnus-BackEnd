198417cba0927f3d4a1e6bcb97e916dcb6252911
Vitor Squina <vitor122703@gmail.com>
2025-04-26 01:34:33 -0300

    Criação de Cadastro de usuario

---
+++ src/main/java/com/farmagnus/farmagnus/user/User.java
@@ -0,0 +1,40 @@
+package com.farmagnus.farmagnus.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@Table(name = "users_farmacia")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome_empresa;
+
+    private String nome_responsavel;
+
+    private String cnpj;
+
+    private String telefone;
+
+    private String email;
+
+    private String senha;
+
+    public User(UserRequestDto data){
+        this.nome_empresa = data.nome_empresa();
+        this.nome_responsavel = data.nome_responsavel();
+        this.cnpj = data.cnpj();
+        this.telefone = data.telefone();;
+        this.email = data.email();
+        this.senha = data.senha();
+    }
+}
+
+
---
+++ src/test/java/com/farmagnus/farmagnus/FarmagnusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmagnus.farmagnus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmagnusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
